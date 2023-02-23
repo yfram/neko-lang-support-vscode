255eb6357cb29ca8c8ec7e73f37499705951ef42
Yoni <yonifram4@gmail.com>
2023-02-23 19:48:02 +0200

    Refactor directory tree

---
+++ src/syntax.ts
@@ -0,0 +1,27 @@
+export const functions = [
+    {
+        label: "$print(arg)",
+        documentation: "Prints the value of the argument to the console.",
+        insertText: "$print",
+    },
+    {
+        label: "$int(arg)",
+        documentation: "Converts the argument to an integer.",
+        insertText: "$int",
+    },
+    {
+        label: "$float(arg)",
+        documentation: "Converts the argument to a float.",
+        insertText: "$float",
+    },
+    {
+        label: "$string(arg)",
+        documentation: "Converts the argument to a string.",
+        insertText: "$string",
+    },
+    {
+        label: "$bool(arg)",
+        documentation: "Converts the argument to a boolean.",
+        insertText: "$bool",
+    },
+];
