ed4b8e0b86f456df70a93719dd0e561fa29ea935
Yoni <yonifram4@gmail.com>
2023-02-23 20:14:10 +0200

    Refactor

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,6 @@
 import * as vscode from 'vscode';
 import { CompletionItem } from 'vscode';
-import { functions } from './syntax';
+import { syntax } from './syntax';
 
 class NekoCompletionItemProvider implements vscode.CompletionItemProvider {
 	provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken, context: vscode.CompletionContext):
@@ -8,14 +8,14 @@ class NekoCompletionItemProvider implements vscode.CompletionItemProvider {
 		const line = document.lineAt(position.line).text;
 		const lineTillCurrentPosition = line.substring(0, position.character + 1);
 		let ret: CompletionItem[] = [];
-		functions.forEach(element => {
+		syntax.forEach(list => list.items.forEach(element => {
 			if (element.label.startsWith(lineTillCurrentPosition)) {
-				let item = new CompletionItem(element.label, vscode.CompletionItemKind.Function);
+				let item = new CompletionItem(element.label, list.kind);
 				item.documentation = element.documentation;
 				item.insertText = element.insertText.replace(lineTillCurrentPosition, "");
 				ret.push(item);
 			}
-		});
+		}));
 		return ret;
 	}
 }
@@ -24,6 +24,5 @@ export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(vscode.languages.registerCompletionItemProvider("plaintext", new NekoCompletionItemProvider(), "$"));
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {
 }
--- src/syntax.ts
+++ src/syntax.ts
@@ -1,27 +1,36 @@
-export const functions = [
-    {
-        label: "$print(arg)",
-        documentation: "Prints the value of the argument to the console.",
-        insertText: "$print",
-    },
-    {
-        label: "$int(arg)",
-        documentation: "Converts the argument to an integer.",
-        insertText: "$int",
-    },
-    {
-        label: "$float(arg)",
-        documentation: "Converts the argument to a float.",
-        insertText: "$float",
-    },
-    {
-        label: "$string(arg)",
-        documentation: "Converts the argument to a string.",
-        insertText: "$string",
-    },
-    {
-        label: "$bool(arg)",
-        documentation: "Converts the argument to a boolean.",
-        insertText: "$bool",
-    },
-];
+import { CompletionItemKind } from "vscode";
+
+const functions = {
+    kind: CompletionItemKind.Function,
+    items:
+        [
+            {
+                label: "$print(arg)",
+                documentation: "Prints the value of the argument to the console.",
+                insertText: "$print",
+            },
+            {
+                label: "$int(arg)",
+                documentation: "Converts the argument to an integer.",
+                insertText: "$int",
+            },
+            {
+                label: "$float(arg)",
+                documentation: "Converts the argument to a float.",
+                insertText: "$float",
+            },
+            {
+                label: "$string(arg)",
+                documentation: "Converts the argument to a string.",
+                insertText: "$string",
+            },
+            {
+                label: "$bool(arg)",
+                documentation: "Converts the argument to a boolean.",
+                insertText: "$bool",
+            },
+        ]
+};
+
+export const syntax = [functions];
+
